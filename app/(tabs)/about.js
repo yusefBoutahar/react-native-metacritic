576f884d2e1984b8296b66494a948098c1d9a52b
yusefBoutahar <yusef_boutahar@hotmail.com>
2024-08-15 19:45:22 +0100

    about WIP

--- app/(tabs)/about.js
+++ app/(tabs)/about.js
@@ -1,10 +1,27 @@
-import { ScrollView } from "react-native";
+import { ScrollView, View, Text } from "react-native";
 import { Screen } from "../../components/Screen";
 
 export default function About() {
+  const className = "text-white";
+
   return (
     <Screen>
-      <ScrollView className="h-full bg-black"></ScrollView>
+      <ScrollView>
+        <View>
+          <Text className={`${className}`}>Sobre el Autor</Text>
+          <Text className={`${className}`}>Nombre: Yousuf Boutahar</Text>
+          <Text className={`${className}`}>
+            Biografía: Yousuf Boutahar es un desarrollador de software con
+            experiencia en una variedad de tecnologías y plataformas. Ha
+            trabajado en proyectos que van desde aplicaciones móviles hasta
+            sistemas backend complejos. Es apasionado por la tecnología y
+            siempre está buscando aprender nuevas cosas.
+          </Text>
+          <Text className={`${className}`}>
+            Contacto: yusef_boutahar@hotmail.com
+          </Text>
+        </View>
+      </ScrollView>
     </Screen>
   );
 }
